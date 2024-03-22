2f3a9da5f8e6c2f7dbdc7fd3faba1594c8e3c321
Denis Malniev <kissed03@gmail.com>
2024-03-22 22:51:17 +0200

    upd common.scss

--- src/modules/react/calculator.jsx
+++ src/modules/react/calculator.jsx
@@ -8,7 +8,10 @@ const Calculator = () => {
   
   return (
   <div className='container'> 
-    <h2 className='bg-card'></h2>
+    <h2 className='bg-card br-16-to-24 p-lr-12-56 p-tb-16-to-32 mb-44-to-68 '>Розрахувати вартість</h2>
+    <div>
+      <input type="radio" />
+    </div>
     <p>Hello, World! {state}</p>
     <button onClick={()=>setState(state+1)}>increase</button>
     <button onClick={()=>setState(state-1)}>decrese</button>

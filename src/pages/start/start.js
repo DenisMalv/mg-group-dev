c4b9da7c024308f5dea8286e86493d67577b66b2
Denis Malniev <kissed03@gmail.com>
2024-03-27 00:18:57 +0200

    custom swiper dynamic pagination

--- src/pages/start/start.js
+++ src/pages/start/start.js
@@ -3,6 +3,9 @@ import './start.scss';
 //common
 import '../../modules/common/google-map';
 
+// swiper
+import '../../modules/common/swiper'
+
 // modules
 import '../../modules/start/some';
 import '../../modules/start/refs';
@@ -13,6 +16,8 @@ import '../../modules/start/header';
 
 // react calculator
 import '../../modules/react/startpage/index'
+
+
 
 
 

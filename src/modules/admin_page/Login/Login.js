2badc646cdc27e57335f20695312033b6b83ca0f
Denis Malniev <kissed03@gmail.com>
2024-04-12 19:07:56 +0300

    test admin panel

---
+++ src/modules/admin_page/Login/Login.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react"
+import { Link, NavLink } from "react-router-dom"
+
+import { useNavigate } from "react-router-dom";
+
+
+
+const Login = () =>{
+
+
+    return( 
+    <div>
+        Login Page
+       
+    </div>
+    )
+}
+
+export default Login

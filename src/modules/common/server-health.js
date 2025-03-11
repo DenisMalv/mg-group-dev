2a343c7ad182cfbe5e4ed8960af560c462d2c7d6
Denis Malniev <kissed03@gmail.com>
2025-03-12 00:00:30 +0200

    Update server-health.js

--- src/modules/common/server-health.js
+++ src/modules/common/server-health.js
@@ -1,4 +1,5 @@
 function wakeUp() {
+	console.log("------------- wake up --------------");
 	fetch("https://mg-group-post-api.onrender.com/work")
 		.then((response) => {
 			if (!response.ok) {
@@ -7,12 +8,17 @@ function wakeUp() {
 			return response.json();
 		})
 		.then((data) => {
+			console.log("------------- i am wake up --------------");
 			console.log("front fetch complete:", data);
 		})
 		.catch((error) => {
+			console.log("------------- i am sleep --------------");
 			console.error("front fetch error:", error);
-		});
+		})
+		.finally(() => {});
 }
+wakeUp();
+
 setInterval(() => {
 	wakeUp();
 }, 60 * 60 * 1000); // 60 минут в миллисекундах

e46ac587a4e14c5639c5f83002a12d879eac6891
Denis Malniev <kissed03@gmail.com>
2025-03-11 21:10:08 +0200

    upd calc, add recaptcha

--- src/modules/common/refsCommon.js
+++ src/modules/common/refsCommon.js
@@ -1,37 +1,34 @@
-const refsCommon ={
-    header: document.getElementById('header'),
-    headerNav:document.querySelector('.header-navigation'),
-    headerList:document.getElementById('header-list'),
-    headerBurgerBtn:document.getElementById('burger-btn'),
-    burgerModal: document.getElementById('burger-modal'),
-    burgerList:document.getElementById('burger-list'),
+const refsCommon = {
+	header: document.getElementById("header"),
+	headerNav: document.querySelector(".header-navigation"),
+	headerList: document.getElementById("header-list"),
+	headerBurgerBtn: document.getElementById("burger-btn"),
+	burgerModal: document.getElementById("burger-modal"),
+	burgerList: document.getElementById("burger-list"),
 
+	//last-our work section
+	lastWorksForm: document.getElementById("last-works-form"),
+	swiperComponentWrapper: document.querySelector(".swiper-component-wrapper"),
+	swiperWrapper: document.querySelector(".swiper-wrapper"),
+	pagination: document.querySelector(".swiper-pagination"),
 
-    //last-our work section
-    lastWorksForm: document.getElementById('last-works-form'),
-    swiperComponentWrapper: document.querySelector('.swiper-component-wrapper'),
-    swiperWrapper:document.querySelector('.swiper-wrapper'),
-    pagination: document.querySelector('.swiper-pagination'),
+	//modals
+	popupOpenBtn: document.getElementById("open-popup-btn"),
+	popupModal: document.getElementById("popup-modal"),
+	popupResp: document.getElementById("popup-resp"),
+	popupForm: document.getElementById("popup-form"),
+	popupFormInputName: document.getElementById("popup-form-name"),
+	popupFormInputPhone: document.getElementById("popup-form-phone"),
+	popupCloseBtn: document.getElementById("popup-close"),
+	popupSubmitBtn: document.getElementById("modalSubmit"),
+	recaptcha: document.querySelector("#capthcaError"),
 
-    //modals
-    popupOpenBtn: document.getElementById('open-popup-btn'),
-    popupModal:document.getElementById('popup-modal'),
-    popupResp:document.getElementById('popup-resp'),
-    popupForm:document.getElementById('popup-form'),
-    popupFormInputName:document.getElementById('popup-form-name'),
-    popupFormInputPhone:document.getElementById('popup-form-phone'),
-    popupCloseBtn:document.getElementById('popup-close'),
-    recaptcha:document.querySelector('#capthcaError'),
+	//sections
+	sectionAboutUs: document.getElementById("about-us"),
+	sectionServices: document.getElementById("services"),
+	sectionGallery: document.getElementById("last-works"),
+	sectionPrice: document.getElementById("calculator"),
+	sectionContacts: document.getElementById("footer"),
+};
 
-    //sections
-    sectionAboutUs:document.getElementById('about-us'),
-    sectionServices:document.getElementById('services'),
-    sectionGallery:document.getElementById('last-works'),
-    sectionPrice:document.getElementById('calculator'),
-    sectionContacts:document.getElementById('footer'),
-    
-}
-
-
-
-export default refsCommon
+export default refsCommon;
--- src/modules/react/Calculator/StepFourFormModalContent.js
+++ src/modules/react/Calculator/StepFourFormModalContent.js
@@ -1,50 +1,107 @@
-import React,{useRef, useEffect, useState} from 'react';
+import React, { useRef, useEffect, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
-import Icon from '../IconSprite';
+import Icon from "../IconSprite";
 
-const StepFourFormModalContent = ({title, descr, data,updData, isShow, setIsShow, name, phone, error, setError,errorMessage})=>{
+const StepFourFormModalContent = ({
+	title,
+	descr,
+	data,
+	updData,
+	isShow,
+	setIsShow,
+	name,
+	phone,
+	captcha,
+	setCaptchaToken,
+	error,
+	setError,
+	errorMessage,
+}) => {
+	useEffect(() => {
+		setTimeout(() => {
+			setIsShow(true);
+		}, 20);
+	}, []);
 
- 
+	return (
+		<>
+			{/* <h4 className={`calc-form-title ${isShow ? 'show' : ''}`}>{title}</h4> */}
+			<p>{descr}</p>
 
-    useEffect(()=>{
-        setTimeout(()=>{setIsShow(true)},20)
-    },[])
+			<div className="calc-select-wrapper" style={{ opacity: 1 }}>
+				<div className={`calc-select-label ${isShow ? "show" : "hide"}`}>
+					<span className="calc-select-label-text">Ім’я</span>
 
-    return(
-        <>
-            {/* <h4 className={`calc-form-title ${isShow ? 'show' : ''}`}>{title}</h4> */}
-            <p>{descr}</p>
+					<div className={`calc-select-input-wrapper`}>
+						<input
+							className={`calc-select-input ${error[name] ? "error" : ""}`}
+							name={name}
+							id={name}
+							type="text"
+							onChange={(e) => updData(name, e.target.value)}
+							value={data[name]}
+							placeholder="Введіть ваше ім’я"
+							autoComplete="off"
+						/>
 
-           <div className='calc-select-wrapper' style={{opacity:1}}>
-                <div className={`calc-select-label ${isShow ? 'show' : 'hide'}`} >
-                    <span className='calc-select-label-text'>Ім’я</span>
-                
-                    <div className={`calc-select-input-wrapper`}> 
+						<Icon
+							classlist={`calc-select-input-icon ${error[name] ? "error" : "hide"}`}
+							id={`${name}-icon`}
+							name="warning"
+							color="#DD7A02"
+							width="24"
+							height="24"
+						/>
+					</div>
+					<p className={`error-message ${error[name] ? "error" : ""}`}>{errorMessage}</p>
+				</div>
+				<div className={`calc-select-label ${isShow ? "show" : "hide"}`}>
+					<span className="calc-select-label-text">Телефон</span>
 
-                        <input className={`calc-select-input ${error[name] ? 'error' : ''}`} name={name} id={name} type='text' onChange={(e)=>updData(name,e.target.value)} value={data[name]} placeholder='Введіть ваше ім’я' autoComplete='off'/>
-                        
-                        <Icon classlist={`calc-select-input-icon ${error[name] ? 'error' : 'hide'}`} id={`${name}-icon`} name="warning" color="#DD7A02" width="24" height="24"/>
+					<div className={`calc-select-input-wrapper`}>
+						<input
+							className={`calc-select-input ${error[phone] ? "error" : ""}`}
+							name={phone}
+							id={phone}
+							type="tel"
+							onChange={(e) => updData(phone, e.target.value)}
+							value={data[phone]}
+							placeholder="Куди вам подзвонити"
+							autoComplete="off"
+						/>
 
-                    </div>
-                    <p className={`error-message ${error[name] ? 'error' : ''}`}>{errorMessage}</p>
-                    
-                </div>
-                <div className={`calc-select-label ${isShow ? 'show' : 'hide'}`} >
-                    <span className='calc-select-label-text'>Телефон</span>
-                
-                    <div className={`calc-select-input-wrapper`}> 
+						<Icon
+							classlist={`calc-select-input-icon ${error[phone] ? "error" : "hide"}`}
+							id={`${phone}-icon`}
+							name="warning"
+							color="#DD7A02"
+							width="24"
+							height="24"
+						/>
+					</div>
+					<p className={`error-message ${error[phone] ? "error" : ""}`}>
+						{"Введіть номер телефону у форматі +380123456789"}
+					</p>
+				</div>
+				<div className={`calc-select-label ${isShow ? "show" : "hide"}`}>
+					<div
+						className={`calc-select-input-wrapper`}
+						style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+					>
+						<ReCAPTCHA
+							sitekey="6LcVT_AqAAAAADCdK3OQ_gu-EjTbE2Z7BM_4mgS6"
+							onChange={(token) => {
+								setCaptchaToken(token);
+								setError((prevState) => ({ ...prevState, captcha: false })); // Убираем ошибку при прохождении
+							}}
+						/>
+					</div>
+					<p className={`error-message ${error["captcha"] ? "error" : ""}`}>{"Підтвердіть що ви людина"}</p>
+				</div>
+			</div>
+		</>
+	);
+};
 
-                        <input className={`calc-select-input ${error[phone] ? 'error' : ''}`} name={phone} id={phone} type='tel' onChange={(e)=>updData(phone,e.target.value)} value={data[phone]} placeholder='Куди вам подзвонити' autoComplete='off'/>
-                        
-                        <Icon classlist={`calc-select-input-icon ${error[phone] ? 'error' : 'hide'}`} id={`${phone}-icon`} name="warning" color="#DD7A02" width="24" height="24"/>
-
-                    </div>
-                    <p className={`error-message ${error[phone] ? 'error' : ''}`}>{'Введіть номер телефону у форматі +380123456789'}</p>
-                    
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default StepFourFormModalContent
+export default StepFourFormModalContent;

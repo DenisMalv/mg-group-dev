a871e40c3b786b2672948ac01f223139aae1c181
Denis Malniev <kissed03@gmail.com>
2025-03-11 23:25:33 +0200

    add fetch interval

---
+++ src/modules/common/server-health.js
@@ -0,0 +1,16 @@
+function fetchData() {
+	fetch("https://mg-group-post-api.onrender.com/work")
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error(`front fetch error: ${response.status}`);
+			}
+			return response.json();
+		})
+		.then((data) => {
+			console.log("front fetch complete:", data);
+		})
+		.catch((error) => {
+			console.error("front fetch error:", error);
+		});
+}
+setInterval(fetchData, 60 * 60 * 1000); // 60 минут в миллисекундах
